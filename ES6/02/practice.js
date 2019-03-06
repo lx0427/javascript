4c11242d765b0e026c0f6ea370873a97571a834d
xiaofeixong <jimforlx@gmail.com>
2019-03-06 13:11:49 +0800

    save

---
+++ ES6/02/practice.js
@@ -0,0 +1,31 @@
+/* practice 1 */
+// function sum({
+//   a = 0,
+//   b = 0
+// } = {}) {
+//   return a + b
+// }
+
+// let thesum = sum({
+//   a: 1,
+//   b: 2
+// })
+// thesum
+
+
+/* practice 2 */
+// function sum([a = 0, b = 0]) {
+//   return a + b
+// }
+
+// let thesum = sum([1, 2])
+// thesum
+
+
+/* practice 3 */
+function sum(a = 0, b = 0) {
+  return a + b
+}
+
+let thesum = sum(1, 2)
+thesum
